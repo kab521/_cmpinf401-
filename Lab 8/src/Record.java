95a745f760a1b57c5ca5516cf75a476dcb00a397
kab521 <112437063+kab521@users.noreply.github.com>
2022-11-09 17:53:30 -0500

    Add files via upload

---
+++ Lab 8/src/Record.java
@@ -0,0 +1,18 @@
+public class Record {
+	private String name;
+	private double time;
+	
+	public Record(String name, double time) {
+		this.name = name;
+		this.time = time;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getTime() {
+		return this.time;
+	}
+
+}
