f619e8819dc9afa13e1292d1fa370f90afefedc8
kab521 <112437063+kab521@users.noreply.github.com>
2022-11-15 00:05:13 -0500

    Add files via upload

---
+++ Lab 9/src/lab9/Driver.java
@@ -0,0 +1,12 @@
+//driver for task 1 - circle/cylinder
+package lab9;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Cylinder c = new Cylinder();
+		System.out.println("This is the volume: " + c.getVolume());
+
+	}
+
+}
---
+++ Lab 9/src/lab9/Person.java
@@ -0,0 +1,24 @@
+package lab9;	
+
+public abstract class Person {
+	protected String name;
+	protected String address = "String";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
