cbb593a5996a85db22538f6766a9b7af0ae4e89b
kab521 <112437063+kab521@users.noreply.github.com>
2022-11-23 16:57:34 -0500

    Add files via upload

--- src/assignment1/Dessert.java
+++ src/assignment1/Dessert.java
@@ -6,30 +6,35 @@
 
 package assignment1;
 public class Dessert {
-	private String name;
-	private String description;
-	private int calories = 0;
-	public Dessert(String name, String description, int cal) {
-		this.name = name;
-		this.description = description;
-		this.calories = cal;
+private String name;
+private String description;	
+private int calories = 0;
+public Dessert(String name, String description, int cal) {
+this.name = name;
+this.description = description;
+this.calories = cal;
 	}
+
+//before running, it made me have to make the method into a constructor, or else it wouldn't run (lines 19-20) (Same with every meal)
+public Dessert() {
+// TODO Auto-generated constructor stub
+}
 public String getName() {
-	return name;
-	}
+return name;
+}
 public void setName(String name) {
-	this.name = name;
-	}
+this.name = name;
+}
 public String getDescription() {
-	return description;
-	}
+return description;
+}
 public void setDescription(String description) {
-	this.description = description;
-	}
+this.description = description;
+}
 public int getCalories() {
-	return calories;
-	}
+return calories;
+}
 public void setCalories(int calories) {
-	this.calories = calories;
-	}
+this.calories = calories;
+}
 }
