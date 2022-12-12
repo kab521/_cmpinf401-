f7257ccee2392ed34f5aa05b798ce65da3a2d1b0
kab521 <112437063+kab521@users.noreply.github.com>
2022-12-12 13:30:58 -0500

    Add files via upload

---
+++ kab521_MenuManager_v3/assignment3/src/kab521_MenuManager_v3/Dessert.java
@@ -0,0 +1,11 @@
+package kab521_MenuManager_v3;
+
+//copy n paste to rest of subclasses:
+//extend
+public class Dessert extends MenuItem{
+public Dessert(String name, String description, int cal, double price) {
+super(name, description, cal, price);
+}
+public Dessert() {
+}
+}
